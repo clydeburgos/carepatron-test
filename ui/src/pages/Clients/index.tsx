7e214d9afe8665cc74abdf114d014c7d9a391d12
Vincent Clyde Burgos <clydeburgos>
2022-08-14 06:07:10 +0800

    Add modal component and initial updates on the main page

--- ui/src/pages/Clients/index.tsx
+++ ui/src/pages/Clients/index.tsx
@@ -1,9 +1,21 @@
 import { memo, useContext, useEffect } from "react";
-import { Paper, Typography } from "@mui/material";
+import { FormControl, InputAdornment, InputLabel, OutlinedInput, Paper, Typography } from "@mui/material";
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
 import { StateContext } from "../../store/DataProvider";
 import Page from "../../components/Page";
 import ClientTable from "./ClientTable";
 import { getClients } from "../../services/api";
+import ClientDetails from "./ClientDetails";
+import SearchIcon from '@mui/icons-material/Search';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 function Clients() {
   const { state, dispatch } = useContext(StateContext);
@@ -20,6 +32,23 @@ function Clients() {
       <Typography variant="h4" sx={{ textAlign: "start" }}>
         Clients
       </Typography>
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+        <FormControl fullWidth sx={{ m: 1 }} size="small">
+          <InputLabel htmlFor="outlined-adornment-search">Search for clients</InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-search"
+            // value={values.amount}
+            // onChange={handleChange('amount')}
+            endAdornment={<InputAdornment position="end"> <SearchIcon></SearchIcon> </InputAdornment>}
+            label="Search for clients"
+          />
+        </FormControl>
+        </Grid>
+        <Grid item xs={4}>
+            <ClientDetails />
+        </Grid>
+      </Grid>
       <Paper sx={{ margin: "auto", marginTop: 3 }}>
         <ClientTable clients={clients} />
       </Paper>
